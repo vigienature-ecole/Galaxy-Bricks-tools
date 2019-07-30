9175886a05d1426794953d6a5444ea5a6b306697
sbenateau <sbenateau@mailodie.fr>
2019-07-30 13:38:21 +0200

    fix column selection, typo and script name

--- tools/LinearRegression.R
+++ tools/LinearRegression.R
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 # tool for simple linear regression
 #
 #  Input :
@@ -16,23 +18,12 @@ args = commandArgs(trailingOnly=TRUE)
 library(data.table) # for data import
 
 # import input file (tabular or csv)
-input = fread(args[1])
+input = data.frame(fread(args[1]))
 
 # run linear model
-res <- lm(input[args[2]] ~ input[args[3]])
+res <- lm(input[ ,as.numeric(args[2])] ~ input[ ,as.numeric(args[3])])
 # get output from linear model
 results <- summary(res)
-
-# add sentence to translate strength of correlation
-# Not implemented yet
-rsquared <- results$r.squared
-
-if (rsquared < .2^2){link = "no or very weak link"} else if(rsquared < .4^2){link = "weak link"
-} else if(rsquared <  .6^2){link = "medium link"
-} else if(rsquared <  .8^2){link = "strong link"
-} else {link = "very strong link"
-}
-
 
 # Output 1 and 2
 capture.output(results, file="mod-summary.txt")
@@ -40,6 +31,6 @@ capture.output(results, file="mod-summary.txt")
 # Output 3 Graph
 # plot data and add trend line
 png('output-plot.png')
-plot(input[args[2]], inputTest[args[3]])
+plot(input[ ,as.numeric(args[2])], input[ ,as.numeric(args[3])])
 abline(res)
 invisible(dev.off())
