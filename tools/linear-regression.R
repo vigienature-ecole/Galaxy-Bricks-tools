1d2d4d7f4ce05b73e033579806627b0d3759ae39
sbenateau <sbenateau@mailodie.fr>
2019-07-31 10:40:07 +0200

    add multiple anova

--- tools/linear-regression.R
+++ tools/linear-regression.R
@@ -14,15 +14,29 @@
 #get arguments from galaxy xlm command
 args = commandArgs(trailingOnly=TRUE)
 
+
 # import package
 library(data.table) # for data import
 
 # import input file (tabular or csv)
 input = data.frame(fread(args[1]))
 
-# run linear model
+multiple = grepl(",", args[3])
 
-formulaMod <- as.formula(paste(colnames(input)[as.numeric(args[2])]," ~ " ,colnames(input)[as.numeric(args[3])]))
+# Build formula
+if (multiple) {
+  varExpl <- as.numeric(unlist(strsplit(args[3], ",")))
+  formulaMod <- as.formula(paste(
+    colnames(input)[as.numeric(args[2])],
+    " ~ " ,
+    paste(colnames(input)[varExpl], collapse = ' + '))
+  )
+  
+} else {
+  formulaMod <- as.formula(paste(colnames(input)[as.numeric(args[2])]," ~ " , colnames(input)[as.numeric(args[3])]))
+}
+
+# run linear model
 res <- lm(formulaMod, data = input)
 
 # get output from linear model
@@ -31,9 +45,11 @@ results <- summary(res)
 # Output 1 and 2
 capture.output(results, file="mod-summary.txt")
 
-# Output 3 Graph
-# plot data and add trend line
-png('output-plot.png')
-plot(formulaMod, data = input)
-abline(res, lwd = 2)
-invisible(dev.off())
+if (!multiple) {
+  # Output 3 Graph
+  # plot data and add trend line
+  png('output-plot.png')
+  plot(formulaMod, data = input)
+  abline(res, lwd = 2)
+  invisible(dev.off())
+}
