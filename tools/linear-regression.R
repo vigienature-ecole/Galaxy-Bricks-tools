bc63cfeb1ec3098a54ead05a4a6f785437f052fd
sbenateau <sbenateau@mailodie.fr>
2020-02-29 17:28:59 +0100

    add translation of output and switch to ggplot tool

--- tools/linear-regression.R
+++ tools/linear-regression.R
@@ -13,13 +13,14 @@
 
 #get arguments from galaxy xlm command
 args = commandArgs(trailingOnly=TRUE)
-
+#args = c("tools/test-data/irisPlus.tabular", "1","2")
 
 # import package
 library(data.table) # for data import
 
 # import input file (tabular or csv)
 input = data.frame(fread(args[1]))
+
 
 multiple = grepl(",", args[3])
 
@@ -42,14 +43,44 @@ res <- lm(formulaMod, data = input)
 # get output from linear model
 results <- summary(res)
 
-# Output 1 and 2
-capture.output(results, file="mod-summary.txt")
+test <- function (x){
+  effet = "false"
+  if (x >= .05) {
+    effet <- " n'a pas d'effet significatif"
+  } else {
+    effet <- " a un effet significatif"
+  }
+  effet
+}
 
-if (!multiple) {
+if (nrow(results[[4]]) > 1) {
+  explanation <- c()
+  for (i in 2:(nrow(results[[4]])))
+    explanation <- c(explanation, paste0("La variable ", rownames(results[[4]])[i], test(results[[4]][i, "Pr(>|t|)"]),
+                                         " sur la variable ", colnames(input)[as.numeric(args[2])],
+                                         ". La probabilité critique est égale à ", round(results[[4]][i, "Pr(>|t|)"], 3), "."))
+  explanation <- c(explanation,
+                   paste0("Le coefficient de détermination (R2) est égal à : ", results[[9]]),
+                   "Attention, ce résultat doit être vérifié.",
+                   "Il peut, par exemple, être la conséquence d'un trop petit échantillon ou d'une confusion d'effet.")
+  fileConn<-file("mod-summary.txt")
+  writeLines(explanation, fileConn)
+  close(fileConn)
+} else {
+  # Output 1 and 2
+  capture.output(results, file="mod-summary.txt")
+}
+
+if (multiple == FALSE){
+  mappingCoord = ggplot2::aes_string(x = names(input)[as.numeric(args[3])],
+                                     y = names(input)[as.numeric(args[2])])
   # Output 3 Graph
-  # plot data and add trend line
-  png('output-plot.png')
-  plot(formulaMod, data = input)
-  abline(res, lwd = 2)
-  invisible(dev.off())
+  # plot data
+  plot_out <- ggplot2::ggplot(input, mappingCoord) +
+    ggplot2::geom_point() +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(axis.text = ggplot2::element_text(size=12),
+                   axis.title = ggplot2::element_text(size=16),
+                   strip.text.x = ggplot2::element_text(size = 14))
+  suppressMessages(ggplot2::ggsave("output-plot.png", plot = plot_out, device = "png"))
 }
