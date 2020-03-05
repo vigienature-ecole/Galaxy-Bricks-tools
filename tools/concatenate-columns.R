6247b44a8f294f7698d93ee9a48eafe78ad1a541
sbenateau <sbenateau@mailodie.fr>
2020-03-05 14:21:52 +0100

    add new tool concatenate

---
+++ tools/concatenate-columns.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+#get arguments from galaxy xlm command
+args = commandArgs(trailingOnly=TRUE)
+#args = c("tools/test-data/irisPlus.tabular", "5", "6", "NewName")
+
+# import input file (tabular or csv)
+input = data.frame(data.table::fread(args[1]))
+
+#concatenate columns
+concatenatedColumns <- paste0(input[, as.numeric(args[2])], input[, as.numeric(args[3])])
+
+#add column to the data.frame
+output <- data.frame(input, concatenatedColumns)
+
+#rename output column
+colnames(output)[ncol(output)] <- args[4]
+
+data.table::fwrite(output, file = "output-concatenate.tabular", sep = ",")
