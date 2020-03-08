997e115c1317f6b636b879cf567621b319f7742f
sbenateau <sbenateau@mailodie.fr>
2020-03-09 00:23:06 +0100

    add options to graph

--- tools/ggplotLite.R
+++ tools/ggplotLite.R
@@ -10,6 +10,7 @@
 
 #get arguments from galaxy xlm command
 args = commandArgs(trailingOnly=TRUE)
+#args <- c("tools/test-data/irisPlus.tabular", "5", "1", "NuageDePoints", "pchou", "", "tesd", "6", "5")
 
 # import package
 library(data.table) # for data import
@@ -20,21 +21,42 @@ input = data.frame(fread(args[1]))
 
 type = args[4]
 
+
+if (args[9] != "") facetVar = names(input)[as.numeric(args[9])] else facetVar = NULL
+if (args[8] != "") colVar = names(input)[as.numeric(args[8])] else colVar = NULL
+
 mappingCoord = ggplot2::aes_string(x = names(input)[as.numeric(args[2])],
                           y = names(input)[as.numeric(args[3])])
 
 if (type == "NuageDePoints"){
-  repType = ggplot2::geom_point()
+  repType = ggplot2::geom_point(aes_string(color =  colVar))
 } else if (type == "DiagrammeEnBarre"){
-  repType = ggplot2::geom_col()
+  repType = ggplot2::geom_col(aes_string(fill =  colVar))
 } else if (type == "BoitesDeDispersion"){
-  repType = ggplot2::geom_boxplot()
+  repType = ggplot2::geom_boxplot(aes_string(color =  colVar))
 } else if (type == "Densite"){
   repType = ggplot2::geom_hex()
 }
 
+if (args[6] != ""){
+  xlabContent = args[6]
+} else {
+  xlabContent = colnames(input)[as.numeric(args[2])]
+}
+
+if (args[7] != ""){
+  ylabContent = args[7]
+} else {
+  ylabContent = colnames(input)[as.numeric(args[3])]
+}
+
+
 plot_out <- ggplot2::ggplot(input, mappingCoord) +
   repType +
+  ggtitle(args[5]) +
+  xlab(xlabContent) +
+  ylab(ylabContent) +
+  facet_wrap(facetVar)
   ggplot2::theme_minimal() +
   ggplot2::theme(axis.text=element_text(size=12),
                    axis.title=element_text(size=16),
