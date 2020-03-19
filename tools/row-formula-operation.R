3ced354a5a248dd47aa375695cd290ca30f5f0e6
sbenateau <sbenateau@mailodie.fr>
2020-03-19 20:24:45 +0100

    Bring together row-simple-operation and row-formula-operation (WIP)

--- tools/row-formula-operation.R
+++ tools/row-formula-operation.R
@@ -6,10 +6,11 @@
 ###########################################
 
 library(data.table)
+library(stringr)
 
 #get arguments from galaxy xlm command
 args = commandArgs(trailingOnly=TRUE)
-args= c("tools/test-data/irisPlus.tabular", "c1 + c2", "test")
+#args= c("tools/test-data/irisPlus.tabular", "c1 + c2", "test")
 
 # load data and formula from form
 inputData <- fread(args[1])
@@ -26,36 +27,36 @@ for (i in seq_along(functions)) evalFormula <- gsub(functions[i],"",evalFormula)
 resultEval <- grepl("^[0-9 c+^/*%(). -- -]+$",evalFormula)
 
 if (resultEval){
-  
+
   if (inputName == "") inputName = inputFormula
-  
+
   # change column names to column code to match formula syntax
   transformedData <- inputData
   colnames(transformedData) <- c(paste0("c",1:ncol(inputData)))
-  
+
   whereToAdd <- str_locate_all(inputFormula, "c[0-9][0-9][0-9]|c[0-9][0-9]|c[0-9]")
   EndFormula <- inputFormula
   for (i in 1:nrow(whereToAdd[[1]])){
-    
+
     frontReplacementInFormula <- gsub(paste0('^(.{',whereToAdd[[1]][i,1]-1 + (i-1) * 21,'})(.*)$'), '\\1transformedData[ ,"\\2', EndFormula)
     totalReplacementInFormula <- gsub(paste0('^(.{',whereToAdd[[1]][i,2] + 19 + (i-1) * 21,'})(.*)$'), '\\1"]\\2', frontReplacementInFormula)
     EndFormula <- gsub(pattern = '"', replacement = "'", totalReplacementInFormula, fixed = TRUE)
   }
-  
+
   #strsplit(inputFormula, split = " ")
-  
+
   # change the formula
   #formulaIndexed <- gsub("c", "transformedData$c",inputFormula)
-  
+
   # apply formula to the data
   resultData <- data.frame(eval(parse(text=EndFormula)))
-  
+
   # change the name of the result column
   colnames(resultData) <- inputName
-  
+
   # add column to original file
   resultData <- data.frame(inputData, resultData)
-  
+
   # write output file
   fwrite(resultData, file = "result.csv", sep = ",")
 } else {
---
+++ tools/row-simple-operation.R
@@ -0,0 +1,55 @@
+#######################################"
+#    SIMPLE OPERATIONS ON LINES
+#    apply operation on the choosen columns of a dataset
+#    Date 19.03.2020
+#    Author : Simon Benateau
+###########################################
+
+library(data.table)
+
+#get arguments from galaxy xlm command
+args = commandArgs(trailingOnly=TRUE)
+#args <- c("tools/test-data/irisPlus.tabular", "1,2,3,4", "5,6", "moyenne", "test", "output")
+#args <- c("tools/test-data/irisPlus.tabular", "1,2,3,4", "5,6", "somme", "test", "valeurs-superieures-a-zero", "test2", "output")
+
+
+#determine the number of loop count
+totalLoop = (length(args) - 4) / 2
+
+# import input file (tabular or csv)
+input = data.frame(data.table::fread(args[1]))
+
+#select columns for the operation(s)
+columnOperationNbr = as.numeric(unlist(strsplit(args[2] , split = ",")))
+columnOperation = input[ , columnOperationNbr]
+#select final dataset
+columnSelectNbr = as.numeric(unlist(strsplit(args[3] , split = ",")))
+finalDataset = input[ , columnSelectNbr]
+
+
+for (i in 1:totalLoop){
+  result = NA
+  # parameters
+  operation = args[4 + (i-1) * 2]
+  outputName = args[5 + (i-1) * 2]
+  
+  if (operation == "moyenne") {
+    result <- rowMeans(columnOperation, na.rm = TRUE)
+  } else if (operation == "somme"){
+    result <- rowSums(columnOperation, na.rm = TRUE)
+  } else if (operation == "ecart-type"){
+    result <- rowSds(columnOperation, na.rm = TRUE)
+  } else if (operation == "valeurs-superieures-a-zero"){
+    columnOperationZero <- columnOperation
+    columnOperationZero[columnOperationZero > 0] <- 1
+    result <- rowSums(columnOperationZero, na.rm = TRUE)
+  }
+  
+  finalDataset <- data.frame(finalDataset, result)
+  colnames(finalDataset)[ncol(finalDataset)] <- outputName
+  
+}
+
+
+# write output file
+fwrite(finalDataset, file = "result.csv", sep = ",")
