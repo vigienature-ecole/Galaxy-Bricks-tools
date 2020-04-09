63a10138414460a7a2aee7447761a851d5e7ca7c
sbenateau <sbenateau@mailodie.fr>
2020-04-09 15:18:42 +0200

    change column summary input options

--- tools/columns-summary.R
+++ tools/columns-summary.R
@@ -11,11 +11,14 @@
 
 #get arguments from galaxy xlm command
 args = commandArgs(trailingOnly=TRUE)
-#args= c("tools/test-data/irisPlus.tabular", "1","youhou","somme", 'output')
-#args= c("tools/test-data/irisPlus.tabular", "1","youhou","mediane", "1","tada","moyenne", 'output')
-
-#determine the number of loop count
-totalLoop = (length(args) - 2) / 3
+# args= c("tools/test-data/irisPlus.tabular", "1", "somme", 'output')
+# args= c("tools/test-data/irisPlus.tabular", "1", "moyenne", 'output')
+# args= c("tools/test-data/irisPlus.tabular", "1", "ecartType", 'output')
+# args= c("tools/test-data/irisPlus.tabular", "1", "ValeursSuperieuresAZero", 'output')
+# args= c("tools/test-data/irisPlus.tabular", "1,2", "somme", 'output')
+# args= c("tools/test-data/irisPlus.tabular", "1,2", "moyenne", 'output')
+# args= c("tools/test-data/irisPlus.tabular", "1,2", "ecartType", 'output')
+# args= c("tools/test-data/irisPlus.tabular", "1,2", "ValeursSuperieuresAZero", 'output')
 
 ## Import file
 input <- data.frame(data.table::fread(args[1]))
@@ -23,24 +26,37 @@ input <- data.frame(data.table::fread(args[1]))
 ## Create final size dataframe
 result <- data.frame((matrix(NA, ncol = 0, nrow = 1)))
 
-## Add new column
-for (i in 1:totalLoop){
-  
-  columnSelected <- as.numeric(args[2 + (i-1) * 3])
-  outputName <- args[3 + (i-1) * 3]
-  outputOperation <- args[4 + (i-1) * 3]
-  
+columnsSelected <- as.numeric(unlist(strsplit(args[2] , split = ",")))
+outputOperation <- args[3]
+
+#make new column names
+columnNames <- paste(outputOperation, colnames(input)[columnsSelected], sep = "_")
+
+if (length(columnsSelected) == 1){
   if (outputOperation == "moyenne"){
-    result[1, outputName] <- mean(input[ ,columnSelected], na.rm = TRUE)
+    result[1, 1] <- mean(input[ , columnsSelected], na.rm = TRUE)
   } else if ( outputOperation == "somme"){
-    result[1, outputName] <- sum(input[ , columnSelected], na.rm = TRUE)
+    result[1, 1] <- sum(input[ , columnsSelected], na.rm = TRUE)
   } else if ( outputOperation == "ecartType"){
-    result[1, outputName] <- sd(input[ ,columnSelected], na.rm = TRUE)
+    result[1, 1] <- sd(input[ ,columnsSelected], na.rm = TRUE)
   } else if ( outputOperation == "ValeursSuperieuresAZero"){
-    inputSelectZero <- input[ ,columnSelected]
+    inputSelectZero <- input[ ,columnsSelected]
     inputSelectZero[inputSelectZero > 0] <- 1
-    result[1, outputName] <- sum(inputSelectZero, na.rm = TRUE)
+    result[1, 1] <- sum(inputSelectZero, na.rm = TRUE)
+  }
+} else {
+  if (outputOperation == "moyenne"){
+    result[1, seq_along(columnsSelected)] <- colMeans(input[ , columnsSelected], na.rm = TRUE)
+  } else if ( outputOperation == "somme"){
+    result[1, seq_along(columnsSelected)] <- colSums(input[ , columnsSelected], na.rm = TRUE)
+  } else if ( outputOperation == "ecartType"){
+    result[1, seq_along(columnsSelected)] <-  apply(input[ ,columnsSelected], 2, function(x) sd(x, na.rm = TRUE))
+  } else if ( outputOperation == "ValeursSuperieuresAZero"){
+    inputSelectZero <- input[ ,columnsSelected]
+    inputSelectZero[inputSelectZero > 0] <- 1
+    result[1, seq_along(columnsSelected)] <- colSums(inputSelectZero, na.rm = TRUE)
   }
 }
-  
-  write.table(result, file="result.csv", row.names=FALSE, sep=",")
+colnames(result) <- columnNames
+
+write.table(result, file="result.csv", row.names=FALSE, sep=",")
