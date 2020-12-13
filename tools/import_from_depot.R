c951dd42b3d675fccf9711922f85780a93995be9
sbenateau <sbenateau@mailodie.fr>
2020-12-13 15:15:08 +0100

    clean import script

--- tools/import_from_depot.R
+++ tools/import_from_depot.R
@@ -1,15 +1,6 @@
 args = commandArgs(trailingOnly=TRUE)
 
-#args <- c("Oiseaux_des_jardins", "tools/query/RequeteVdtVNE4.sql", "tools/query/RequeteOiseauxVNE4.sql", "tools/query/RequeteEscargotsVNE4.sql", "tools/query/RequeteSauvagesVNE4.sql")
-#args <- c("Operation_escargots", "tools/query/RequeteVdtVNE4.sql", "tools/query/RequeteOiseauxVNE4.sql", "tools/query/RequeteEscargotsVNE4.sql", "tools/query/RequeteSauvagesVNE4.sql")
-
-#args <- c("Oiseaux_des_jardins")
-#args <- c("Vers_de_terre")
-#args <- c("Operation_escargots")
-#args <- c("Sauvages_de_ma_rue")
-#args <- c("INPN")
-
-
+# choose between observatories
 if (args[1] == "Vers_de_terre"){
   file <- "vdt.csv"
 } else if (args[1] == "Oiseaux_des_jardins"){
@@ -22,22 +13,28 @@ if (args[1] == "Vers_de_terre"){
   file <- "INPN.csv"
 }
 
+# get data set
 URL_data_VNE <- RCurl::getURL(paste0("https://depot.vigienature-ecole.fr/datasets/bricks/", file), .encoding = "UTF-8")
 data_VNE <- read.csv(text = URL_data_VNE, encoding = 'UTF-8')
+# write file
+write.csv(data_VNE, "output-importVNE.csv", row.names = FALSE)
 
-parseJSONLabelValue <- function (df, var) {
-  # parse JSON
-  parsedData <- jsonlite::stream_in(textConnection(df[ , var]))
-  # get data.frame
-  flattenData <- jsonlite::flatten(parsedData)
-  # donner les bons noms de champs
-  colnames(flattenData)[seq(2, ncol(flattenData), 2)] <- flattenData[1, seq(1, ncol(flattenData) - 1, 2)]
-  # selectionner les colonnes
-  flattenData <- flattenData[ , seq(2, ncol(flattenData), 2)]
-  # passer les TRUE FALSE en 0 1
-  flattenData <- sapply(flattenData, as.numeric)
-  as.data.frame(flattenData)
-}
+
+
+
+# parseJSONLabelValue <- function (df, var) {
+#   # parse JSON
+#   parsedData <- jsonlite::stream_in(textConnection(df[ , var]))
+#   # get data.frame
+#   flattenData <- jsonlite::flatten(parsedData)
+#   # donner les bons noms de champs
+#   colnames(flattenData)[seq(2, ncol(flattenData), 2)] <- flattenData[1, seq(1, ncol(flattenData) - 1, 2)]
+#   # selectionner les colonnes
+#   flattenData <- flattenData[ , seq(2, ncol(flattenData), 2)]
+#   # passer les TRUE FALSE en 0 1
+#   flattenData <- sapply(flattenData, as.numeric)
+#   as.data.frame(flattenData)
+# }
 
 # if (args[1] == "Sauvages_de_ma_rue"){
 #   #   cleaned_df <- data_VNE[!is.na(data_VNE$environnement), ]
@@ -91,6 +88,5 @@ parseJSONLabelValue <- function (df, var) {
 #   data_VNE$composition_zone <- NULL
 # }
 
-# write file
-write.csv(data_VNE, "output-importVNE.csv", row.names = FALSE)
+
 
