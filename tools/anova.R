2b2772ec4ee2b0c47edd09e6def4870ab30d4754
sbenateau <sbenateau@mailodie.fr>
2021-02-09 11:29:08 +0100

    add multiple anova

--- tools/anova.R
+++ tools/anova.R
@@ -21,7 +21,7 @@ args = commandArgs(trailingOnly=TRUE)
 # args = c("tools/test-data/irisPlus.tabular", "4", "5")
 # args = c("../../Downloads/Résumer des données on data 1.csv", "3", "2")
 # args = c("../../Downloads/Données_INPN.csv.csv", "24", "17")
-
+# args = c("../../Downloads/somme sur la colonne 11 en fonction de la colonne 1,10,12.csv(1).csv", "4", "3,2")
 
 # import input file (tabular or csv)
 input = data.frame(data.table::fread(args[1]))
@@ -33,7 +33,10 @@ varExplNames <- colnames(input)[varExpl]
 
 
 # clean dataset
-input = input[!input[ ,varExplNames] == "", ]
+for (i in seq_along(varExplNames)){
+  input = input[!input[ ,varExplNames[i]] == "", ]
+  input = input[!is.na(input[ ,varExplNames[i]]), ]
+}
 
 # transform value to factors
 input[varExplNames] <- lapply(input[varExplNames], factor)
@@ -103,39 +106,70 @@ graphRes <- function(input, varExplNames, varDepName){
                             y = max(indicateurMeanMax$ICmax) + .1 * (max(indicateurMeanMax$ICmax) - min(min(indicateurMeanMax$ICmin))), 
                             label = vectorLetter)
     
-  } 
-  # traduction
-  
-  letterGroup <- unique(unlist(strsplit(as.character(vectorLetter), "")))
-  resTuckey = "Résultat du test statistique : \n \n"
-  
-  if (results[[1]]$`Pr(>F)`[1] > 0.05){
-    resTuckey <- paste(resTuckey, "La variable ", varExplNames, " n'a pas d'effet significatif sur la variable ", varDepName,". \n \n", "Les catégories ", levelNamesFormated, " ne sont pas significativement différentes.", sep = "")
-  } else {
-    resTuckey <- paste(resTuckey, "La variable ", varExplNames, " a un effet significatif sur la variable ", varDepName," \n \n", sep = "")
     
-    for (i in 1:length(letterGroup)){
-      positionLetters = grep(pattern = letterGroup[i], vectorLetter)
-      if (length(positionLetters) > 1){
-        resTuckey <- paste(resTuckey, "Les moyennes ne sont significativement pas différente pour les catégories :", paste(names(vectorLetter[positionLetters]),"\n", collapse = ", "))
-      } else {
-        resTuckey <- paste(resTuckey, "La catégorie", names(vectorLetter[positionLetters]), "est significativement différente de toutes les autres \n")
+    # traduction
+    
+    letterGroup <- unique(unlist(strsplit(as.character(vectorLetter), "")))
+    resTuckey = "Résultat du test statistique : \n \n"
+    
+    if (results[[1]]$`Pr(>F)`[1] > 0.05){
+      resTuckey <- paste(resTuckey, "La variable ", varExplNames, " n'a pas d'effet significatif sur la variable ", varDepName,". \n \n", "Les catégories ", levelNamesFormated, " ne sont pas significativement différentes.", sep = "")
+    } else {
+      resTuckey <- paste(resTuckey, "La variable ", varExplNames, " a un effet significatif sur la variable ", varDepName," \n \n", sep = "")
+      
+      for (i in 1:length(letterGroup)){
+        positionLetters = grep(pattern = letterGroup[i], vectorLetter)
+        if (length(positionLetters) > 1){
+          resTuckey <- paste(resTuckey, "Les moyennes ne sont significativement pas différente pour les catégories :", paste(names(vectorLetter[positionLetters]),"\n", collapse = ", "))
+        } else {
+          resTuckey <- paste(resTuckey, "La catégorie", names(vectorLetter[positionLetters]), "est significativement différente de toutes les autres \n")
+        }
+      }
+      #add warning if a variable is in more than one group
+      # get variables in 2 groups
+      variablesInTwoGroups <- which(nchar(as.character(vectorLetter))>1)
+      if (length(variablesInTwoGroups) > 1){
+        print(paste("Attention les catégories :", names(vectorLetter[variablesInTwoGroups]), "sont présentes dans plusieurs groupes"))
+      } else if (length(variablesInTwoGroups) == 1){
+        print(paste("Attention la catégorie :", names(vectorLetter[variablesInTwoGroups]), "est présente dans plusieurs groupes"))
       }
     }
-    #add warning if a variable is in more than one group
-    # get variables in 2 groups
-    variablesInTwoGroups <- which(nchar(as.character(vectorLetter))>1)
-    if (length(variablesInTwoGroups) > 1){
-      print(paste("Attention les catégories :", names(vectorLetter[variablesInTwoGroups]), "sont présentes dans plusieurs groupes"))
-    } else if (length(variablesInTwoGroups) == 1){
-      print(paste("Attention la catégorie :", names(vectorLetter[variablesInTwoGroups]), "est présente dans plusieurs groupes"))
+    
+    plot + labs(caption = resTuckey) + 
+      theme(
+        plot.caption = element_text(hjust = 1, size = 12)
+      )
+    
+  } else if (length(varExplNames) == 2){
+    plot <- ggplot(indicateurMeanMax, aes_string(x = varExplNames[1], y = "mean")) +
+      geom_point() +
+      geom_errorbar(aes(ymin = ICmin, ymax = ICmax), width = .2) +
+      facet_grid(as.formula(paste("~", varExplNames[2]))) +
+      theme_minimal() +
+      theme(legend.position = "none") +
+      theme(axis.text.x=element_text(angle = 35, hjust = 1))+
+      ylab(varDepName)
+    
+    
+    resMultiple = "Résultat du test statistique : \n \n"
+    
+    for (i in 1:2){
+      if (results[[1]]$`Pr(>F)`[i] > 0.05){
+        levelNamesFormated <- unique(input[, varExplNames[i]])
+        resMultiple <- paste(resMultiple, "La variable ", row.names(results[[1]])[i], " n'a pas d'effet significatif sur la variable ", varDepName,". \n ", "Les catégories ", levelNamesFormated, " ne sont pas significativement différentes.\n \n", sep = "")
+      } else {
+        levelNamesFormated <- unique(input[, varExplNames[i]])
+        resMultiple <- paste(resMultiple, "La variable ", varExplNames[i], " a un effet significatif sur la variable ", varDepName," \n ", "Au moins une des catégories est significativement différente des autres.\n \n", sep = "")
+      }
     }
+    plot + labs(caption = resMultiple) + 
+      theme(
+        plot.caption = element_text(hjust = 1, size = 12)
+      )
+  } else {
+    stop("Il ne peut pas y avoir plus de 2 varibles explicatives pour le moment")
   }
-  
-  plot + labs(caption = resTuckey) + 
-          theme(
-            plot.caption = element_text(hjust = 1, size = 12)
-          )
+
 }
 
 
