bf710f3ee784ec227e4f47a7ebbb02f8947f14a4
sbenateau <sbenateau@mailodie.fr>
2021-03-12 15:16:02 +0100

    add spipoll as dataset

--- tools/import_from_depot.R
+++ tools/import_from_depot.R
@@ -1,5 +1,5 @@
 args = commandArgs(trailingOnly=TRUE)
-
+encoding = "UTF-8"
 # choose between observatories
 if (args[1] == "Vers_de_terre"){
   file <- "vdt.csv"
@@ -9,6 +9,9 @@ if (args[1] == "Vers_de_terre"){
   file <- "escargots.csv"
 } else if(args[1] == "Sauvages_de_ma_rue"){
   file <- "sauvages.csv"
+} else if(args[1] == "Spipoll"){
+  file <- "spipoll.csv"
+  encoding = "Latin-1"
 } else if (args[1] == "INPN"){
   file <- "DataINPN.csv"
 } else if (args[1] == "INPN_Gasteropodes"){
@@ -19,7 +22,8 @@ if (args[1] == "Vers_de_terre"){
 
 # get data set
 URL_data_VNE <- RCurl::getURL(paste0("https://depot.vigienature-ecole.fr/datasets/bricks/", file), .encoding = "UTF-8")
-data_VNE <- read.csv(text = URL_data_VNE, encoding = 'UTF-8')
+
+data_VNE <- data.table::fread(text = URL_data_VNE, fill = TRUE, encoding = encoding)
 
 if (args[1] == "INPN" & args[2] != "saisons"){
   data_VNE <- data_VNE[ , c(
