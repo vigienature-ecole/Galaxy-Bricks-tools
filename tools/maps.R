dc7076d69ed1d99462eb4ac2b667984a5d90fa9b
sbenateau <sbenateau@mailodie.fr>
2021-03-25 14:28:15 +0100

    improve map tool

--- tools/maps.R
+++ tools/maps.R
@@ -1,52 +1,54 @@
 # carto dans galaxy
 
-args = commandArgs(trailingOnly=TRUE)
-
-# args = c("../../../Downloads/compte sur la colonne 2 en fonction de la colonne 9.csv.csv",
-#          "1",
-#          "departements",
-#          "2",
-#          "data/maps/departements-version-simplifiee.geojson",
-#          "data/maps/regions-version-simplifiee.geojson",
-#          "data/maps/metropole-version-simplifiee.geojson",
-#          "data/maps/correspondance_departement_region.csv")
-
 library(sf)
 library(dplyr)
 library(ggplot2)
+
+args = commandArgs(trailingOnly=TRUE)
+
 
 # get parameters
 input <- args[1]
 geographic_data <- as.numeric(args[2])
 geographic_scale <- args[3]
 variable <- as.numeric(args[4])
-#correspondance_departement_region <- args[8]
 
 #load data
 data_file <- data.table::fread(input)
 # remove spaces
 colnames(data_file)[variable] <- gsub(" ", "_", colnames(data_file)[variable])
 
-#correspondance_departement_region_df <- data.table::fread(correspondance_departement_region)
-
 data_file_geo <- data_file
-data_file_geo[[geographic_data]] <- substring(as.vector(data_file_geo[[geographic_data]]), 0, 2)
 
+# add 0 to departement where they are missing
+short_values <- nchar(as.vector(data_file_geo[[geographic_data]])) < 2
+
+data_file_geo[[geographic_data]][short_values] <- paste0("0", as.vector(data_file_geo[[geographic_data]])[short_values])
+
+if (any(data_file_geo[[geographic_data]] > 5)){
+  name = TRUE
+} else {
+  name = FALSE
+}
+
+if (geographic_scale != "points"){
+  if (name) {
+    colnames(data_file_geo)[geographic_data] <- "nom"
+    data_file_geo$code <- as.character(data_file_geo$code)
+  } else {
+    colnames(data_file_geo)[geographic_data] <- "code"
+    data_file_geo$code <- as.character(data_file_geo$code)
+  }
+}
 
 #TODO : verifier qu'il n'y a qu'une valeur par données sinon afficher une erreur
 #charger les données de position
 if (geographic_scale == "departements") {
-  colnames(data_file_geo)[geographic_data] <- "code"
-  data_file_geo$code <- as.character(data_file_geo$code)
   geographic_scale_to_load =  args[5]
 } else if (geographic_scale == "regions") {
-  colnames(data_file_geo)[geographic_data] <- "code"
-  data_file_geo$code <- as.character(data_file_geo$code)
-  data_file_geo <- dplyr::left_join( data_file_geo, correspondance_departement_region_df, by=c("code" = "code_departement"))
-  data_file_geo <- data_file_geo[, -"code"]
-  colnames(data_file_geo)[which(colnames(data_file_geo) == "code_region")] <- "code"
-  data_file_geo$code <- as.character(data_file_geo$code)
   geographic_scale_to_load =  args[6]
+} else if (geographic_scale == "academies") {
+  geographic_scale_to_load =  args[7]
 } else if (geographic_scale == "points"){
   geographic_scale_to_load =  args[7]
   data_file_geo <- st_as_sf(data_file_geo,coords=c("longitude", "latitude")) %>%
@@ -59,7 +61,11 @@ geo = sf::read_sf(geographic_scale_to_load)
 
 # join data with geographic data
 if (geographic_scale != "points") {
-  geo <- dplyr::left_join(geo, data_file_geo, by="code")
+  if (name){
+    geo <- dplyr::left_join(geo, data_file_geo, by="nom")
+  } else {
+    geo <- dplyr::left_join(geo, data_file_geo, by="code")
+  }
 }
 # transform to lamber 93 projection
 geo <- st_transform(geo, 2154)
@@ -67,7 +73,7 @@ geo <- st_transform(geo, 2154)
 # add grid
 g = st_graticule(geo)
 
-
+print("ok")
 
 png("carte.png", width = 800, height = 800)
 par(mar = c(0,0,0,0))
